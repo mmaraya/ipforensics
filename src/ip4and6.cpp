0fd128044979c6cc3bf52e5ffedc9b622ea2a09b
Mike Maraya <mike.maraya@gmail.com>
2014-01-04 13:37:32 -0500

    Added IPForensics class

---
+++ src/ip4and6.cpp
@@ -0,0 +1,9 @@
+//
+//  ip4and6.cpp
+//  ipforensics
+//
+//  Created by Michael Maraya on 1/4/14.
+//  Copyright (c) 2014 Random Dent. All rights reserved.
+//
+
+#include "ip4and6.h"
---
+++ src/ip4and6.h
@@ -0,0 +1,14 @@
+//
+//  ip4and6.h
+//  ipforensics
+//
+//  Created by Michael Maraya on 1/4/14.
+//  Copyright (c) 2014 Random Dent. All rights reserved.
+//
+
+#ifndef __ipforensics__ip4and6__
+#define __ipforensics__ip4and6__
+
+#include <iostream>
+
+#endif /* defined(__ipforensics__ip4and6__) */
