6460b8a84f72db5e59df9200ec7bcf283dc0e2c0
Mike Maraya <mike.maraya@gmail.com>
2014-02-10 23:02:09 -0500

    comments for Packet class

--- src/packet.h
+++ src/packet.h
@@ -1,30 +1,42 @@
-//
-// The MIT License (MIT)
-//
-// Copyright (c) 2014 Michael Maraya
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//
+/**
+ *  @file packet.h
+ *  @brief Packet class definitions
+ */
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Michael Maraya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #include <iostream>
 #include "host.h"
 
+/**
+ *  @brief Model class for storing information about a single network node,
+ *         following the model-view-controller software design pattern
+ *  @details Host stores the media access control, Internet Protocol version 4,
+ *           and Internet Protocol version 6 address information for a single
+ *           network node.
+ */
 class Packet {
 private:
   MACAddress mac_src_;
@@ -47,5 +59,13 @@ public:
   IPv6Address ipv6_dst() const;
 };
 
+/**
+ *  @brief Provide the std::string representation of a Packet by overloading the
+ *         << operator for std::ostream
+ *  @param out std::ostream output stream
+ *  @param p Packet instance to display as an std::string
+ *  @retval std::ostream address that contains the std::string representation of
+ *          this Packet
+ */
 std::ostream &operator<<(std::ostream &out, const Packet &p);
 
